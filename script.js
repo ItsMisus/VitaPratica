5a97c4ab39820301dcaa5c68c08ece2c869330de
ItsMisus <francesco.minutiello08@gmail.com>
2025-12-03 22:57:59 +0100

    ciaone

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleMenu() {
     document.querySelector('.nav-categories').classList.toggle('active');
-    document.querySelector('.menu-bar').classList.toggle('active');
     document.querySelector('.user-links').classList.toggle('active');
 }
