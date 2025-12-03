519c389e40b9f98a712e4502d1dcb56f2a0dcd66
ItsMisus <francesco.minutiello08@gmail.com>
2025-12-03 22:50:40 +0100

    adsadas

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    document.querySelector('.nav-categories').classList.toggle('active');
+    document.querySelector('.menu-bar').classList.toggle('active');
+    document.querySelector('.user-links').classList.toggle('active');
+}
